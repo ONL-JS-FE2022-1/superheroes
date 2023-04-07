793e6c2a4fafd12040674650fe1b09e1be32de58
Roman Ratushnyi <freshjs.ofe@gmail.com>
2023-04-07 18:30:37 +0300

    Create server

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const app = require('./app');
+const {PORT} = require('./config');
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    `Server started on ${PORT}`
+});
