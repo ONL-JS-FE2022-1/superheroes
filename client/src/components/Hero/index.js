879a18d478f4d839b055af9d6aac176789693f2c
Roman Ratushnyi <freshjs.ofe@gmail.com>
2023-04-11 20:29:28 +0300

    Add slider for hero images

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styles from './Hero.module.css';
 
-const Hero = ({hero}) => {
+const Hero = ({ hero }) => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }
+
     return (
         <article>
             <h1>{hero.nickname}</h1>
             <h2>Also known as {hero.realName}</h2>
-            {hero.images.map((image) => (
-                <img 
-                    key={image.id}
-                    src={`http://localhost:5000/images/${image.path}`}
-                    alt={hero.nickname}
-                />
-            ))}
+            <Slider {...settings}>
+                {hero.images.map((image) => (
+                    <img
+                        key={image.id}
+                        src={`http://localhost:5000/images/${image.path}`}
+                        alt={hero.nickname}
+                    />
+                ))}
+            </Slider>
 
             <p>Catch phrase: {hero.catchPhrase}</p>
             <p>Origin description</p>
