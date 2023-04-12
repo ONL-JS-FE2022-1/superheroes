196aea08833615152517eb9e0e6373666a0fe0ca
Roman Ratushnyi <freshjs.ofe@gmail.com>
2023-04-12 18:48:38 +0300

    Add confirmation modal window

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -6,9 +6,14 @@ import styles from './Hero.module.css';
 import { deleteHero, deletePower } from '../../api';
 import { getHeroes } from '../../redux/slices/heroSlice';
 import { useDispatch } from 'react-redux';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 
 const Hero = ({ hero }) => {
+    const [modalOpen, setModalOpen] = useState(false);
+
     const dispatch = useDispatch();
 
     const settings = {
@@ -22,18 +27,20 @@ const Hero = ({ hero }) => {
     const deleteHandler = async () => {
         await deleteHero(hero.id);
         dispatch(getHeroes());
+        setModalOpen(false);
     }
 
     const deletePowerHandler = async (powerId) => {
         await deletePower(hero.id, powerId);
         dispatch(getHeroes());
+        setModalOpen(false);
     }
 
     return (
         <article>
             <h1>{hero.nickname}</h1>
             <h2>Also known as {hero.realName}</h2>
-            <Slider {...settings} style={{width: "20%", margin: "30px"}}>
+            <Slider {...settings} style={{ width: "20%", margin: "30px" }}>
                 {hero.images.map((image) => (
                     <img
                         key={image.id}
@@ -62,7 +69,30 @@ const Hero = ({ hero }) => {
                 ))}
             </ul>
 
-            <button onClick={deleteHandler}>Delete superhero!</button>
+            <button onClick={() => setModalOpen(true)}>Delete superhero!</button>
+
+            {/* Модальне вікно для підтвердження видалення */}
+            <Modal
+                isOpen={modalOpen}
+                onRequestClose={() => setModalOpen(false)}
+                contentLabel="Delete Hero Modal"
+                style={{
+                    content: {
+                        top: '50%',
+                        left: '50%',
+                        right: 'auto',
+                        bottom: 'auto',
+                        marginRight: '-50%',
+                        transform: 'translate(-50%, -50%)',
+                    }
+                }}
+            >
+                <h2>Delete Hero</h2>
+                <p>Are you sure you want to delete {hero.nickname}?</p>
+                {/* Підтвердження видалення */}
+                <button onClick={deleteHandler}>Yes</button>
+                <button onClick={() => setModalOpen(false)}>No</button>
+            </Modal>
         </article>
     );
 }
