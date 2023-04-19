63c7c32a64bae6e8ad1c4fa6e6c23bf4c08deee8
Roman Ratushnyi <freshjs.ofe@gmail.com>
2023-04-19 20:25:40 +0300

    Add toastify (error)

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,39 @@
 import HeroesPage from "./pages/HeroesPage";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const { error } = useSelector((state) => state.heroes);
+
+  if (error) {
+    toast.error('An error occured!', {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }
   return (
-    <HeroesPage />
+    <>
+      <HeroesPage />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
   );
 }
 
